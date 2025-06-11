0d2be2ceb4b974769e8bce5dbffddb258c625f69
souriosec <souriosec@proton.me>
2025-06-11 15:38:41 +0900

    copilot, colorsheme

--- lua/souriosec/plugins/colorscheme.lua
+++ lua/souriosec/plugins/colorscheme.lua
@@ -1,32 +1,33 @@
 return {
-	"rebelot/kanagawa.nvim",
-	priority = 1000, -- make sure to load this before all the other start plugins
-	config = function()
-		-- Default options:
-		require("kanagawa").setup({
-			compile = false, -- enable compiling the colorscheme
-			undercurl = true, -- enable undercurls
-			commentStyle = { italic = false },
-			functionStyle = {},
-			keywordStyle = { italic = false },
-			statementStyle = { bold = true },
-			typeStyle = {},
-			transparent = false, -- do not set background color
-			dimInactive = false, -- dim inactive window `:h hl-NormalNC`
-			terminalColors = true, -- define vim.g.terminal_color_{0,17}
-			colors = { -- add/modify theme and palette colors
-				palette = {},
-				theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
-			},
-			overrides = function(colors) -- add/modify highlights
-				return {}
-			end,
-			theme = "lotus", -- Load "wave" theme when 'background' option is not set
-			background = { -- map the value of 'background' option to a theme
-				dark = "dragon", -- try "dragon" !
-				light = "lotus",
-			},
-		})
-		vim.cmd("colorscheme kanagawa-dragon")
-	end,
+	{
+		"ellisonleao/gruvbox.nvim",
+		priority = 1000,
+		config = function()
+			require("gruvbox").setup({
+				terminal_colors = true, -- add neovim terminal colors
+				undercurl = true,
+				underline = true,
+				bold = true,
+				italic = {
+					strings = false,
+					emphasis = false,
+					comments = false,
+					operators = false,
+					folds = false,
+				},
+				strikethrough = true,
+				invert_selection = false,
+				invert_signs = false,
+				invert_tabline = false,
+				inverse = true, -- invert background for search, diffs, statuslines and errors
+				contrast = "hard", -- can be "hard", "soft" or empty string
+				palette_overrides = {},
+				overrides = {},
+				dim_inactive = false,
+				transparent_mode = false,
+			})
+			vim.cmd([[colorscheme gruvbox]])
+		end,
+		opts = ...,
+	},
 }
