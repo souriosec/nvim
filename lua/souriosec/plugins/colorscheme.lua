f8abe717f5aff8cf07f1c6ee3f8a609d445678e2
souriosec <souriosec@proton.me>
2026-06-28 14:39:30 +0900

    colors

--- lua/souriosec/plugins/colorscheme.lua
+++ lua/souriosec/plugins/colorscheme.lua
@@ -11,7 +11,7 @@ return {
 					operators = false,
 					folds = false,
 				},
-				contrast = "hard", -- can be "hard", "soft" or empty string
+				contrast = "soft", -- can be "hard", "soft" or empty string
 			})
 			vim.cmd([[colorscheme gruvbox]])
 		end,
